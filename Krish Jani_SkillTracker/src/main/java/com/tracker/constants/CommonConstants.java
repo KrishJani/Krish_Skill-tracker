ad1732adfe02e9057233e4ca24a26af807238e98
Krish Jani <65438720+KrishJani@users.noreply.github.com>
2022-07-19 20:08:12 +0530

    Update CommonConstants.java

--- Krish Jani_SkillTracker/src/main/java/com/tracker/constants/CommonConstants.java
+++ Krish Jani_SkillTracker/src/main/java/com/tracker/constants/CommonConstants.java
@@ -1,12 +1,7 @@
 package com.tracker.constants;
 
-// TODO: Auto-generated Javadoc
-/**
- * The Class CommonConstants.
- */
 public class CommonConstants {
 	
-	/** The Constant SUCCESS_STRING. */
 	public static final String SUCCESS_STRING = "Success";
 
 }
